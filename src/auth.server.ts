f6a1a446d0204b3a03c12064b924b91cc2c83a30
Tom <tom.weelborg@gmail.com>
2025-06-23 01:09:14 +0200

    Add logout logic

--- src/auth.server.ts
+++ src/auth.server.ts
@@ -3,6 +3,8 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import config from './config/config.server';
 
+export const authJsSessionToken = 'authjs.session-token';
+
 const { handle } = SvelteKitAuth({
 	secret: env.AUTH_SECRET,
 	trustHost: Boolean(env.AUTH_TRUST_HOST),
