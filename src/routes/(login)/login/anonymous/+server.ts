2d423f9ced6875aaab9032329d6a60a89a5b5e37
Tom <tom.weelborg@gmail.com>
2025-06-23 22:05:01 +0200

    Add error handling to login process

--- src/routes/(login)/login/anonymous/+server.ts
+++ src/routes/(login)/login/anonymous/+server.ts
@@ -1,18 +1,8 @@
-import { userService } from '$lib/services/UserService';
+import { userService } from '$lib/user/UserService.server';
 import config from '../../../../config/config';
 
 export const GET = async ({ url, cookies }) => {
 	const username: string | null = url.searchParams.get(config.usernameChoiceQueryParam);
 
-	if (!username) {
-		return new Response('Username may not be empty.', {
-			status: 400
-		});
-	}
-
-	const userAuthToken = await userService.getAccessTokenForGuestUser(username);
-
-	cookies.set(config.userAuthTokenCookieKey, userAuthToken, config.userAuthTokenCookieOptions);
-
-	return new Response(userAuthToken);
+	return await userService.getAccessTokenForGuestUser(username, cookies);
 };
