12ced5a49f380c156dde432f1b07e82805a0d9b5
Tom <tom.weelborg@gmail.com>
2025-07-02 22:04:19 +0200

    Encapsulate user access token logic in UserService

--- src/routes/(login)/+layout.server.ts
+++ src/routes/(login)/+layout.server.ts
@@ -1,9 +1,9 @@
-import config from '$lib/config/config';
+import { userService } from '$lib/user/UserService.server';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-	const accessToken = event.cookies.get(config.userAccessTokenCookieKey);
+	const accessToken = userService.getAccessTokenFromCookie(event.cookies);
 
 	if (accessToken) {
 		throw redirect(303, '/');
