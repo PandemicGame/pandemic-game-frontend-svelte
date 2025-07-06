874e8427a43698559728aa9c3ee4d6184edcff5e
Tom <tom.weelborg@gmail.com>
2025-07-06 19:57:52 +0200

    Fix import

--- src/routes/(user)/+layout.server.ts
+++ src/routes/(user)/+layout.server.ts
@@ -1,5 +1,5 @@
 import { userService } from '$lib/user/UserService.server';
-import type { LayoutServerLoad } from '../$types';
+import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
 	userService.refreshAccessTokenCookie(event.cookies);
