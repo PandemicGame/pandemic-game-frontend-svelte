aa53be0c8e182a99ca5fa14cf83151d8c3a49444
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-06 20:54:29 +0200

    Add User WebSocket

--- src/routes/(user)/+layout.svelte
+++ src/routes/(user)/+layout.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import { userService } from '$lib/user/UserService';
 	import { userStore } from '$lib/user/UserStore';
+	import { userWebSocketConnector } from '$lib/user/UserWebSocketConnector.js';
 	import { onMount } from 'svelte';
 
 	let { children, data } = $props();
 
 	const accessToken = data.userAccessToken;
 
-	onMount(async () => {
-		const user = await userService.getUserInfo(accessToken);
-		userStore.set(user);
+	onMount(() => {
+		(async () => {
+			const user = await userService.getUserInfo(accessToken);
+			userStore.set(user);
+		})();
+
+		userWebSocketConnector.connect(accessToken);
+
+		return () => {
+			userWebSocketConnector.disconnect();
+		};
 	});
 </script>
 
