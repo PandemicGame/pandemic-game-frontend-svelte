e2e16d0a65a1bf1a103d61a79328d8b22a1a0d4a
Tom <tom.weelborg@gmail.com>
2025-07-08 21:21:46 +0200

    Add account information

--- src/lib/user/User.type.ts
+++ src/lib/user/User.type.ts
@@ -1,5 +1,8 @@
+import type Account from '$lib/auth/Account.type';
 import ChatMessageSender from '$lib/chat/ChatMessageSender.type';
 import { TypeInfo } from '$lib/websocket/WebSocketMapper';
 
 @TypeInfo('User')
-export default class User extends ChatMessageSender {}
+export default class User extends ChatMessageSender {
+	account?: Account;
+}
