3098f101d9f1c9babe0d80e0afcdf164c779e2a1
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-08 21:21:46 +0200

    Add account information

---
+++ src/lib/auth/Account.type.ts
@@ -0,0 +1,12 @@
+import WebSocketData from '$lib/websocket/WebSocketData';
+import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+
+@TypeInfo('Account')
+export default class Account extends WebSocketData {
+	firstName?: string;
+	lastName?: string;
+	username?: string;
+	email?: string;
+	issuer?: string;
+	subject?: string;
+}
