945aa39a31fa369e4644dbfb43236aff42819f02
Tom <tom.weelborg@gmail.com>
2025-07-08 17:46:02 +0200

    Update main page layout

--- src/routes/(user)/+page.svelte
+++ src/routes/(user)/+page.svelte
@@ -15,13 +15,32 @@
 	});
 </script>
 
-<div class="h-screen w-screen">
-	<h1>You are logged in as: {user?.name}.</h1>
-	<a
-		href="/logout"
-		class="btn preset-filled-error-500 font-bold"
-		data-sveltekit-preload-data="off">
-		Logout
-	</a>
-	<GlobalChat />
+<div class="grid h-screen w-screen grid-cols-[100dvw] grid-rows-[6rem_1fr]">
+	<div class="flex flex-row justify-end px-4 py-2">
+		<div class="flex flex-col gap-2">
+			<h1>You are logged in as: {user?.name}.</h1>
+			<a
+				href="/logout"
+				class="btn preset-filled-error-500 ml-auto font-bold"
+				draggable="false"
+				data-sveltekit-preload-data="off">
+				Logout
+			</a>
+		</div>
+	</div>
+
+	<div class="grid grid-cols-[var(--larmcw)_1fr_var(--larmcw)] gap-x-4 overflow-hidden p-4">
+		<div></div>
+		<div></div>
+		<div class="h-full overflow-hidden">
+			<GlobalChat />
+		</div>
+	</div>
 </div>
+
+<style>
+	:root {
+		--left-and-right-main-content-width: 25rem;
+		--larmcw: var(--left-and-right-main-content-width);
+	}
+</style>
