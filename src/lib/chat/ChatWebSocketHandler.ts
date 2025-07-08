0e9e39832b437e7b574f2b42fc1553f736236975
Tom <tom.weelborg@gmail.com>
2025-07-08 17:39:26 +0200

    Add global chat

--- src/lib/chat/ChatWebSocketHandler.ts
+++ src/lib/chat/ChatWebSocketHandler.ts
@@ -2,7 +2,8 @@ import WebSocketHandler from '$lib/websocket/WebSocketHandler';
 import Chat from './Chat.type';
 import ChatMessage from './ChatMessage.type';
 import { chatService } from './ChatService';
-import { addChatMessage, updateChatStore } from './ChatStore';
+import { addChatMessage, globalChatIdStore, updateChatStore } from './ChatStore';
+import GlobalChat from './GlobalChat.type';
 
 class ChatWebSocketHandler extends WebSocketHandler {
 	public handle(data: unknown): void {
@@ -10,6 +11,9 @@ class ChatWebSocketHandler extends WebSocketHandler {
 			chatService.parseChatMessageDate(data);
 			addChatMessage(data);
 		} else if (data instanceof Chat) {
+			if (data instanceof GlobalChat) {
+				globalChatIdStore.set(data.id);
+			}
 			chatService.addChatMessageSenders(data.members ?? [], data.messages ?? []);
 			if (data.id) {
 				data.messages?.forEach(chatService.parseChatMessageDate);
