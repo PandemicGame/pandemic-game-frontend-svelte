2fec93f20580c97ca81cf857a58e425a28af078c
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-09 14:46:24 +0200

    Add lobby creation

---
+++ src/lib/lobby/LobbyAndAccessTokenHolder.type.ts
@@ -0,0 +1,9 @@
+import WebSocketData from '$lib/websocket/WebSocketData';
+import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+import type Lobby from './Lobby.type';
+
+@TypeInfo('LobbyAndAccessTokenHolder')
+export default class LobbyAndAccessTokenHolder extends WebSocketData {
+	lobby?: Lobby;
+	accessToken?: string;
+}
--- src/lib/user/UserWebSocketConnector.ts
+++ src/lib/user/UserWebSocketConnector.ts
@@ -1,5 +1,6 @@
 import { chatWebSocketHandler } from '$lib/chat/ChatWebSocketHandler';
 import config from '$lib/config/config';
+import { lobbyWebSocketHandler } from '$lib/lobby/LobbyWebSocketHandler';
 import WebSocketConnector from '$lib/websocket/WebSocketConnector';
 import { userWebSocketHandler } from './UserWebSocketHandler';
 
@@ -15,4 +16,5 @@ export function connectAndAssignHandlers(accessToken: string) {
 	userWebSocketConnector.connect(accessToken);
 	userWebSocketConnector.addHandler(userWebSocketHandler);
 	userWebSocketConnector.addHandler(chatWebSocketHandler);
+	userWebSocketConnector.addHandler(lobbyWebSocketHandler);
 }
