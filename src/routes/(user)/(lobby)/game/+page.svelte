0ebff9033f0bf12d16e4052b494e27d8559f04ad
Tom <tom.weelborg@gmail.com>
2025-07-16 18:36:26 +0200

    Update game view

--- src/routes/(user)/(lobby)/game/+page.svelte
+++ src/routes/(user)/(lobby)/game/+page.svelte
@@ -1,5 +1,22 @@
-<script lang="ts"></script>
+<script lang="ts">
+</script>
 
-<div>
-	<h1>Game</h1>
+<div class="grid h-screen w-screen grid-cols-[1fr_auto]">
+	<div class="grid grid-cols-[auto_1fr_auto] grid-rows-[auto_1fr_auto_auto]">
+		<div class="z-10 col-[1_/_span_3] row-[1_/_span_3]">
+			<div class="h-full w-full bg-black">Board</div>
+		</div>
+		<div class="z-20 col-3 row-1">
+			<div class="size-40 bg-black">Plague Cubes</div>
+		</div>
+		<div class="z-20 col-1 row-[3_/_span_2]">
+			<div class="h-80 w-full bg-black">Outbreak Marker</div>
+		</div>
+		<div class="z-10 col-[2_/_span_2] row-4">
+			<div class="h-40 w-full bg-black">Actions</div>
+		</div>
+	</div>
+	<div>
+		<div>Player Information</div>
+	</div>
 </div>
