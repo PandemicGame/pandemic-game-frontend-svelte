1c80f448d9216309ea7c910754e75402ad706364
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-18 22:06:35 +0200

    Remove possibly undefined fields from GameOptions

--- src/lib/game/GameOptions.type.ts
+++ src/lib/game/GameOptions.type.ts
@@ -4,7 +4,11 @@ import type BoardType from './board/BoardType.type';
 
 @TypeInfo()
 export default class GameOptions extends WebSocketData {
-	id?: number;
-	availableBoardTypes?: BoardType[];
-	selectedBoardTypeId?: number;
+	constructor(
+		public id: number,
+		public availableBoardTypes: BoardType[],
+		public selectedBoardTypeId: number
+	) {
+		super();
+	}
 }
