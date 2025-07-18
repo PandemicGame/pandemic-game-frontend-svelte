2f8c4f26f930976164f2ac4e3fe3fbec2d175f33
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-18 23:00:14 +0200

    Remove possibly undefined fields from Color

--- src/lib/util/Color.type.ts
+++ src/lib/util/Color.type.ts
@@ -3,9 +3,13 @@ import { TypeInfo } from '$lib/websocket/WebSocketMapper';
 
 @TypeInfo()
 export default class Color extends WebSocketData {
-	r?: number;
-	g?: number;
-	b?: number;
+	constructor(
+		public r: number,
+		public g: number,
+		public b: number
+	) {
+		super();
+	}
 
 	public getCssValue(): string {
 		return `rgb(${this.r}, ${this.g}, ${this.b})`;
