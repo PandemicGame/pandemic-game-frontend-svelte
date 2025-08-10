46a3de24b9e0e290230b2f06b35a8bfc720ac945
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-11 00:51:35 +0200

    Add Action

--- src/lib/game/turn/Turn.type.ts
+++ src/lib/game/turn/Turn.type.ts
@@ -1,9 +1,13 @@
 import WebSocketData from '$lib/websocket/WebSocketData';
 import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+import type Action from '../action/Action.type';
 
 @TypeInfo()
 export default class Turn extends WebSocketData {
-	constructor(public player: number) {
+	constructor(
+		public player: number,
+		public availableActions: Action[]
+	) {
 		super();
 	}
 }
