5c2a43a65b00296aef6bd63d47319b24c80ce911
Tom <tom.weelborg@gmail.com>
2025-08-13 21:56:51 +0200

    Add executing action effects

--- src/lib/game/GameService.ts
+++ src/lib/game/GameService.ts
@@ -1,9 +1,15 @@
 import Service from '$lib/Service';
+import { lobbyWebSocketConnector } from '$lib/lobby/LobbyWebSocketConnector';
+import type ActionEffect from './action/effects/ActionEffect.type';
 import type Field from './board/Field.type';
 
 class GameService extends Service {
 	public createIdToFieldMap(fields: Field[]): Map<number, Field> {
 		return new Map<number, Field>(fields.map((field) => [field.id, field]));
+	}
+
+	public executeActionEffect(actionEffect: ActionEffect) {
+		lobbyWebSocketConnector.sendMessage('/game/execute-action-effect', String(actionEffect.id));
 	}
 }
 
