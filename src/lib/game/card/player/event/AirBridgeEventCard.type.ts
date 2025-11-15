e9a135db93ec551e89727b0889bbbf8e31ff405e
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-15 19:37:29 +0100

    Add AirBridgeEventCard

---
+++ src/lib/game/card/player/event/AirBridgeEventCard.type.ts
@@ -0,0 +1,14 @@
+import type Color from '$lib/util/Color.type';
+import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+import EventCard from './EventCard.type';
+
+@TypeInfo()
+export default class AirBridgeEventCard extends EventCard {
+	constructor(
+		public title: string,
+		public color: Color,
+		public description: string
+	) {
+		super(title, color, description);
+	}
+}
