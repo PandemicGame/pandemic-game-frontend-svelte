1667debfcea171a15fd4d8cf969ca4a603a84052
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-15 18:08:19 +0100

    Add CarActionForSelf

---
+++ src/lib/game/action/simple/car/CarActionForSelf.type.ts
@@ -0,0 +1,10 @@
+import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+import type MoveActionEffect from '../../effects/MoveActionEffect.type';
+import CarAction from './CarAction.type';
+
+@TypeInfo()
+export default class CarActionForSelf extends CarAction {
+	constructor(public availableEffects: MoveActionEffect[]) {
+		super(availableEffects);
+	}
+}
