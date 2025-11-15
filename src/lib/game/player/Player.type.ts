934d94ddd158018cd08776d10b24e7a3bb1b4baa
Tom <tom.weelborg@gmail.com>
2025-11-15 21:11:53 +0100

    Add hand cards to Player

--- src/lib/game/player/Player.type.ts
+++ src/lib/game/player/Player.type.ts
@@ -1,5 +1,6 @@
 import WebSocketData from '$lib/websocket/WebSocketData';
 import { TypeInfo } from '$lib/websocket/WebSocketMapper';
+import type PlayerCard from '../card/player/PlayerCard.type';
 import type Role from '../role/Role.type';
 
 @TypeInfo()
@@ -8,7 +9,8 @@ export default class Player extends WebSocketData {
 		public id: number,
 		public name: string,
 		public currentField: number,
-		public role: Role
+		public role: Role,
+		public handCards: PlayerCard[]
 	) {
 		super();
 	}
