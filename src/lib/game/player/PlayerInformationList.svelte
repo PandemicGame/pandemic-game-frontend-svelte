65e983a7cd247171a4a09c7a074668e6b9bcbb1e
Tom <tom.weelborg@gmail.com>
2025-11-15 21:16:52 +0100

    Add PlayerInformationList

---
+++ src/lib/game/player/PlayerInformationList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type Player from './Player.type';
+	import PlayerInformation from './PlayerInformation.svelte';
+
+	let { players }: { players: Player[] } = $props();
+</script>
+
+<ul>
+	{#each players as player}
+		<PlayerInformation {player} />
+	{/each}
+</ul>
