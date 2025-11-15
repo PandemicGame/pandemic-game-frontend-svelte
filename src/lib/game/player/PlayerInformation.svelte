df23ec24acbd11aac0e032e7764161756cb9e42c
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-15 21:16:33 +0100

    Add PlayerInformation

---
+++ src/lib/game/player/PlayerInformation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type Player from './Player.type';
+
+	const { player }: { player: Player } = $props();
+</script>
+
+<li class="p-4">
+	<h1 class="text-xl font-semibold">{player.name}</h1>
+	<ul class="ml-2">
+		{#each player.handCards as handCard}
+			<li>
+				{handCard.title}
+			</li>
+		{/each}
+	</ul>
+</li>
