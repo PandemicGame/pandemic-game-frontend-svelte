1f13423e514e411b439b5a1bb82825cfce905751
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-16 17:00:35 +0100

    Make ActionEffect extend Effect

--- src/lib/game/action/effects/ActionEffect.type.ts
+++ src/lib/game/action/effects/ActionEffect.type.ts
@@ -1,9 +1,9 @@
-import WebSocketData from '$lib/websocket/WebSocketData';
+import Effect from '$lib/game/effect/Effect.type';
 import { TypeInfo } from '$lib/websocket/WebSocketMapper';
 
 @TypeInfo()
-export default class ActionEffect extends WebSocketData {
+export default class ActionEffect extends Effect {
 	constructor(public id: number) {
-		super();
+		super(id);
 	}
 }
