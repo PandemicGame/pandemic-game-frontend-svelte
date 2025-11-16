e0fd4ce2f3ef02f4806b0000e8e53cda8c5f3d42
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-16 17:52:10 +0100

    Add logic to store Effect to answer

--- src/lib/game/GameStore.ts
+++ src/lib/game/GameStore.ts
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store';
+import Effect from './effect/Effect.type';
 import type Game from './Game.type';
 import Player from './player/Player.type';
 
 export const currentGame = writable<Game | undefined>();
 
 export const currentPlayer = writable<Player | undefined>();
+
+export const effectToAnswer = writable<Effect | undefined>();
