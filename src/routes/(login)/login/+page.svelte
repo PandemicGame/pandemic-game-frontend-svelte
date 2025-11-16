dfacc5d2f7fd988e1f8c244973dd4a3b35f1692f
Tom <tom.weelborg@gmail.com>
2025-11-16 02:54:54 +0100

    Update Tabs to skeleton version 4

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -19,33 +19,32 @@
 <div class="flex h-screen w-screen items-center justify-center">
 	<div class="h-80 w-lg rounded p-12 shadow-md">
 		<Tabs value={loginMethod} onValueChange={(e) => (loginMethod = e.value)}>
-			{#snippet list()}
-				<Tabs.Control value="anonymous">Anonymous</Tabs.Control>
-				<Tabs.Control value="authenticated">Authenticated</Tabs.Control>
-			{/snippet}
-			{#snippet content()}
-				<Tabs.Panel value="anonymous">
-					<form class="flex flex-col gap-2" onsubmit={submitAnonymousLoginForm}>
-						<label class="label">
-							<span class="label-text">Username</span>
-							<input class="input" type="text" bind:value={username} required />
-						</label>
-						<div>
-							<button type="submit" class="btn preset-filled-primary-500">
-								Choose username
-							</button>
-						</div>
-					</form>
-				</Tabs.Panel>
-				<Tabs.Panel value="authenticated">
-					<a
-						href="/auth/signin"
-						class="btn preset-filled-primary-500"
-						data-sveltekit-preload-data="off">
-						Sign in
-					</a>
-				</Tabs.Panel>
-			{/snippet}
+			<Tabs.List>
+				<Tabs.Trigger value="anonymous">Anonymous</Tabs.Trigger>
+				<Tabs.Trigger value="authenticated">Authenticated</Tabs.Trigger>
+				<Tabs.Indicator />
+			</Tabs.List>
+			<Tabs.Content value="anonymous">
+				<form class="flex flex-col gap-2" onsubmit={submitAnonymousLoginForm}>
+					<label class="label">
+						<span class="label-text">Username</span>
+						<input class="input" type="text" bind:value={username} required />
+					</label>
+					<div>
+						<button type="submit" class="btn preset-filled-primary-500">
+							Choose username
+						</button>
+					</div>
+				</form>
+			</Tabs.Content>
+			<Tabs.Content value="authenticated">
+				<a
+					href="/auth/signin"
+					class="btn preset-filled-primary-500"
+					data-sveltekit-preload-data="off">
+					Sign in
+				</a>
+			</Tabs.Content>
 		</Tabs>
 	</div>
 </div>
